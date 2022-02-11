d866f362dee6d53d916cd6a015a2db32a7c8dd6b
Tomasz Matuszewski <Tomasz_Matuszewski@epam.com>
2022-02-11 15:41:39 +0100

    update

--- conf/creds.sql
+++
@@ -1,7 +0,0 @@
-# admin account 
-# SECURITY: change password after first login
-#
-# TICKET 4342: Security problem with multiple Testlink installations on the same server 
-INSERT INTO /*prefix*/users (login,password,role_id,email,first,last,locale,active,cookie_string)
-			VALUES ('admin',MD5('test'), 8,'', 'Testlink','Administrator', 'en_GB',1,CONCAT(MD5(RAND()),MD5('admin')));
-
