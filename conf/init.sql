f4c09c94e2f3d37787e0e894f62eb1eb63df9fc1
Tomasz Matuszewski <Tomasz_Matuszewski@epam.com>
2022-02-11 15:44:27 +0100

    update

--- conf/init.sql
+++ conf/init.sql
@@ -1145,7 +1145,7 @@ INSERT INTO /*prefix*/role_rights (role_id,right_id) VALUES (9,50);
 #
 # TICKET 4342: Security problem with multiple Testlink installations on the same server 
 INSERT INTO /*prefix*/users (login,password,role_id,email,first,last,locale,active,cookie_string)
-			VALUES ('admin',MD5('admin'), 8,'', 'Testlink','Administrator', 'en_GB',1,CONCAT(MD5(RAND()),MD5('admin')));
+			VALUES ('admin',MD5('$(TESTLINK_PASS)'), 8,'', 'Testlink','Administrator', 'en_GB',1,CONCAT(MD5(RAND()),MD5('admin')));
 
 
 # Assignment types
