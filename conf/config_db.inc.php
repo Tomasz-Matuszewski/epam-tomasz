5d997cb83536d413cda05fe6c00072be70b1a3cf
Tomasz Matuszewski <Tomasz_Matuszewski@epam.com>
2022-02-11 12:30:00 +0100

    Update

--- conf/config_db.inc.php
+++ conf/config_db.inc.php
@@ -1,8 +1,8 @@
 <?php
 define('DB_TYPE', 'mysql');
-define('DB_USER', '${TESTLINK_DB_USER}');
-define('DB_PASS', '${TESTLINK_DB_PASSWORD}');
-define('DB_HOST', '${TESTLINK_DB_HOST}');
-define('DB_NAME', '${TESTLINK_DB_NAME}');
+define('DB_USER', '');
+define('DB_PASS', '');
+define('DB_HOST', '');
+define('DB_NAME', '');
 define('DB_TABLE_PREFIX', '');
 ?>
