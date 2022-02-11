3b853b90d046e5517e6576b77a7aa317e1b7ab60
Tomasz Matuszewski <Tomasz_Matuszewski@epam.com>
2022-02-11 15:08:34 +0100

    Update

--- conf/init.sql
+++ conf/init.sql
@@ -1144,8 +1144,8 @@ INSERT INTO /*prefix*/role_rights (role_id,right_id) VALUES (9,50);
 # SECURITY: change password after first login
 #
 # TICKET 4342: Security problem with multiple Testlink installations on the same server 
-INSERT INTO /*prefix*/users (login,password,role_id,email,first,last,locale,active,cookie_string)
-			VALUES ('admin',MD5('admin'), 8,'', 'Testlink','Administrator', 'en_GB',1,CONCAT(MD5(RAND()),MD5('admin')));
+#INSERT INTO /*prefix*/users (login,password,role_id,email,first,last,locale,active,cookie_string)
+#			VALUES ('admin',MD5('admin'), 8,'', 'Testlink','Administrator', 'en_GB',1,CONCAT(MD5(RAND()),MD5('admin')));
 
 
 # Assignment types
