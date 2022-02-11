2911d7433b88cb79d920d8c3bd4b3f372d868c77
Tomasz Matuszewski <Tomasz_Matuszewski@epam.com>
2022-02-11 12:37:30 +0100

    update

--- conf/config_db.inc.php
+++ conf/config_db.inc.php
@@ -1,8 +1,8 @@
 <?php
 define('DB_TYPE', 'mysql');
-define('DB_USER', '');
-define('DB_PASS', '');
-define('DB_HOST', '');
-define('DB_NAME', '');
+define('DB_USER', getenv('DB_USER'));
+define('DB_PASS', getenv('DB_PASS'));
+define('DB_HOST', getenv('DB_HOST'));
+define('DB_NAME', getenv('DB_NAME'));
 define('DB_TABLE_PREFIX', '');
 ?>
